1657f6fa1ca265783b57bbc65f30f1cf57dafe80
kuwingto <kuwingto@gmail.com>
2018-12-24 16:35:22 +0800

    add test

---
+++ PutImageToBackground/test.cpp
@@ -0,0 +1,21 @@
+#include <opencv2/opencv.hpp>
+#include "PutImageToBackground.h"
+void PutImageToBackgroundTest()
+{
+	cv::Mat Src = loadImage("C:\\Users\\Ronald\\Desktop\\A\\templ1.bmp");
+	cv::imshow("Src", Src);
+	cv::waitKey(1);
+
+	const cv::Size BGSize = cv::Size(200, 200);
+	const unsigned char FillValue = 255;
+	const cv::Point SrcCenterLocInBG = cv::Point(BGSize.width / 2, BGSize.height / 2);
+	cv::Mat Dst;
+	int status = PutImageToBackground(Src,
+		BGSize,
+		FillValue,
+		SrcCenterLocInBG,
+		Dst);
+	cv::imshow("Dst", Dst);
+	cv::waitKey(0);
+	return;
+}
